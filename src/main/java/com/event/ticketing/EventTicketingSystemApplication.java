2cab01806c4771fe4b416f7481ce8f0790aadf56
Prajesh Panjiyar <panjiyarprajesh@yahoo.com>
2023-11-15 12:05:14 +0545

    initial setup

---
+++ src/main/java/com/event/ticketing/EventTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.event.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/ticketing/EventTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
